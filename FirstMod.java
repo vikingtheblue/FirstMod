a6c54431f9a7654abb80975dfc953e82dfbd14cc
vikingtheblue <ryandalecast@gmail.com>
2015-05-27 14:58:38 -0700

    FirstMod.java
    FirstMod Class

---
+++ FirstMod.java
@@ -0,0 +1,19 @@
+package com.vikingtheblue.FirstMod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = FirstMod.MODID, version = FirstMod.VERSION)
+public class FirstMod
+{
+    public static final String MODID = "firstmod";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		
+    }
+}
